da12ad2d8f0b29cc2f9021a8c95f683540d7d508
KurtDev599 <sw536880@naver.com>
2023-11-21 22:34:19 +0900

    open cors

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
+  app.enableCors({
+    origin: ['http://3.34.126.21/', 'http://localhost:3000'],
+    credentials: true,
+    exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+  });
 }
 bootstrap();
