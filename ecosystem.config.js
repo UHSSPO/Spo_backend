a58d6a4ae321d9aa617fca103f82c0b2054800c2
KurtDev599 <sw536880@naver.com>
2023-11-21 22:18:15 +0900

    pm2 minor fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,10 @@ module.exports = {
             autorestart: true, // 프로세스 실패 시 자동으로 재시작할지 선택
             watch: false, // 파일이 변경되었을 때 재시작 할지 선택
             // max_memory_restart: "512M", // 프로그램의 메모리 크기가 일정 크기 이상이 되면 재시작한다.
+	     env: {
+       	     NODE_ENV: 'production',
+             PORT: 3001, // 원하는 포트로 설정
+     	     },
         },
     ]
 };
