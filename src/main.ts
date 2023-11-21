dc99ccf9d72af526ae2a986e7baf40cf0c1f3b6e
KurtDev599 <sw536880@naver.com>
2023-11-21 22:47:36 +0900

    open cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
   app.enableCors({
     origin: ['http://3.34.126.21', 'http://localhost:3000'],
     credentials: true,
   });
-
+  await app.listen(3001);
 }
 bootstrap();
