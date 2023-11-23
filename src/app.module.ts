b44952ec58cb632bc5795eb713ff4c2b64559adc
KurtDev599 <sw536880@naver.com>
2023-11-23 16:26:31 +0900

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingMiddleware } from './common/middleware/logging.middleware';
+import { AuthModule } from './routes/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
