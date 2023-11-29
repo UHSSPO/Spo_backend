161e979da0d00b003f5afb88fe7ebb49c308a80a
KurtDev599 <sw536880@naver.com>
2023-11-29 19:43:08 +0900

    minor

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+// import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { KakaoLoginDto } from './dto/create-user.dto';
+import { Controller } from '@nestjs/common';
+// import { KakaoLoginDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('/kakao-login')
-  async getKakaoInfo(@Body() reqBody: KakaoLoginDto) {
-    return await this.authService.kakaoLogin(
-      reqBody.apikey,
-      reqBody.redirectUri,
-      reqBody.code,
-    );
-  }
+  // @Post('/kakao-login')
+  // async getKakaoInfo(@Body() reqBody: KakaoLoginDto) {
+  //   return await this.authService.kakaoLogin(
+  //     reqBody.apikey,
+  //     reqBody.redirectUri,
+  //     reqBody.code,
+  //   );
+  // }
 }
